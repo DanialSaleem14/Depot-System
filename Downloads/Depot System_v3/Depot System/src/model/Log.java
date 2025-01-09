bc8fff8af23822b1a521efdae3f4fff259a8c202
Danial Saleem <danialsaleemdgk1@gmail.com>
2025-01-09 15:09:38 +0500

    Commit 1

---
+++ Downloads/Depot System_v3/Depot System/src/model/Log.java
@@ -0,0 +1,29 @@
+package model;
+
+/**
+ *
+ * @author dania
+ */
+public class Log {
+    private static Log instance;
+    private StringBuffer logBuffer;
+
+    private Log() {
+        logBuffer = new StringBuffer();
+    }
+
+    public static Log getInstance() {
+        if (instance == null) {
+            instance = new Log();
+        }
+        return instance;
+    }
+
+    public void logEvent(String event) {
+        logBuffer.append(event).append("\n");
+    }
+
+    public String getLog() {
+        return logBuffer.toString();
+    }
+}
